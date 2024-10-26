400652ac35dd4dfb88ce6f400768eefbf13b4c50
Christopher Müller <chris@mechatronikmonkey.de>
2024-10-26 13:55:23 +0200

    Update xdrv_102_DeHumidifier.ino
    Re-Implemented the trailing time as it came to problems.

--- tasmota/tasmota_xdrv_driver/xdrv_102_DeHumidifier.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_102_DeHumidifier.ino
@@ -28,6 +28,14 @@ bool COMP_ON = false;
 int minOffTimer = 0;
 int FanTrailingTimer = 0;
 int water_full = 0;
+int restartProtection = 0;
+bool COMP_ON_LAST = false;
+
+bool COMP_STATE = false;
+bool COMP_REQUS = false;
+
+bool FAN_STATE = false;
+bool FAN_REQUS = false;
 
 /*********************************************************************************************\
  * Dehumidifier Functions
@@ -39,24 +47,11 @@ bool DeHumInitSuccess = false;
 
 const char DeHumCommands[] PROGMEM = "|"  // No Prefix
   "FAN|" 
-  "COMP";
+  "COMP|"
+  "RES";
 
 void (* const DeHumCommand[])(void) PROGMEM = {
-  &CmdFAN, &CmdCOMP};
-
-void SetCompressor_OFF(int fan_trail_time)
-{
-  // Set Fan to low level and set trailing run.
-  digitalWrite(DeHum.pin_fanh, LOW);
-  delay(5);
-  digitalWrite(DeHum.pin_fanl, HIGH);
-  FanTrailingTimer = fan_trail_time; // Set trailing timer for fan to 60 seconds.
-  FAN_SPEED = 1;
-  FAN_ON = true;
-
-  digitalWrite(DeHum.pin_comp, LOW);
-  COMP_ON = false;
-}
+  &CmdFAN, &CmdCOMP, &CmdRES};
 
 void CmdFAN(void) {
 
@@ -83,28 +78,20 @@ void CmdFAN(void) {
     case 0: 
       // For switching off fan, the compressor needs to shut down.
       // Triger Compressor Shutdown
-      SetCompressor_OFF(60);
-
-      Response_P(PSTR("Compressor OFF. FAN set to low... trailing time set to 60 seconds."));
+      FAN_REQUS = false;
+      Response_P(PSTR("Request FAN OFF."));
 
     break;
     case 1:
-      // Set Fan to low level
-      digitalWrite(DeHum.pin_fanh, LOW);
-      delay(5);
-      digitalWrite(DeHum.pin_fanl, HIGH);
       FAN_SPEED = 1;
-      FAN_ON = true;
-      Response_P(PSTR("FAN set to low."));
+      FAN_REQUS = true;
+      Response_P(PSTR("Request FAN set to low."));
       break;
     case 2:
       // Set Fan to high level
-      digitalWrite(DeHum.pin_fanl, LOW);
-      delay(5);
-      digitalWrite(DeHum.pin_fanh, HIGH);
       FAN_SPEED = 2;
-      FAN_ON = true;
-      Response_P(PSTR("FAN set to high."));
+      FAN_REQUS = true;
+      Response_P(PSTR("Request FAN set to high."));
       /* code */
     break;
     
@@ -138,28 +125,47 @@ int comp = 0;
   {
     if (comp == 1)
     {
-      // Set Fan to low level
-      digitalWrite(DeHum.pin_fanh, LOW);
-      delay(5);
-      digitalWrite(DeHum.pin_fanl, HIGH);
-      FAN_ON = true;
-      FAN_SPEED = 1;
+      // Set Fan to high level
 
-      // Set Compressor to ON
-      digitalWrite(DeHum.pin_comp, HIGH);
-      COMP_ON = true;
-      Response_P(PSTR("Compressor ON."));
+      COMP_REQUS = true;
+      Response_P(PSTR("Request Compressor ON."));
     }
     else
     {
-      // Trigger Compressor shutdown with 60 seconds trailing time
-      SetCompressor_OFF(60);
-      Response_P(PSTR("Compressor OFF."));
+      // Trigger Compressor shutdown with 90 seconds trailing time
+      COMP_REQUS = false;
+      Response_P(PSTR("Request Compressor OFF..."));
     }
   }
 }
 
+void CmdRES(void) {
 
+int res = 0;
+
+  if (XdrvMailbox.data_len > 0) {
+  
+    res = (int)(CharToFloat(XdrvMailbox.data));
+  }
+  else
+  {
+    ResponseCmndChar(PSTR("Error. Enter 1 to reset Water and restart!"));
+  }
+  
+  if (res > 1 || res < 0)
+  {
+    // Count out of range
+    ResponseCmndChar(PSTR("Error. Out of range, only 0-1!"));
+  }
+  else
+  {
+    if (res == 1)
+    {
+      water_full = 0;
+      Response_P(PSTR("Water reset. You can start compressor now."));
+    }
+  }
+}
 
 /*********************************************************************************************\
  * Tasmota Functions
@@ -196,36 +202,71 @@ void DeHumInit()
 
 void DeHumProcessing(void)
 {
-  water_full = digitalRead(DeHum.pin_water);
-
-  // is the compressor running and the fan off?
-  if (COMP_ON == true && FAN_ON == false)
-  { 
-    //switch on fan on low
-    digitalWrite(DeHum.pin_fanh, LOW);
-    delay(5);    
-    digitalWrite(DeHum.pin_fanl, HIGH); 
-    FAN_ON = true;
-    FAN_SPEED = 1;
-    Response_P(PSTR("FAN set to low because compressor cannot run without fan."));
-  }
-
-  // Is the compressor running and whater full?
-  if (water_full <= 0 && COMP_ON)
+  // Transitions 
+  if (COMP_REQUS != COMP_ON)
   {
 
-    SetCompressor_OFF(90);
-    Response_P(PSTR("Water full! Stopped operation."));
-
+    // COMP OFF to ON only when restart protection 0
+    // Enable FAN with compressor
+    if (COMP_REQUS == true && restartProtection == 0)
+    {
+      COMP_ON = true;
+      FAN_ON = true;
+    }
+    
+    if (COMP_REQUS == false)
+    {
+      COMP_ON = false;
+      FAN_REQUS = false; // Not off immediatelly but after trailing timer
+      restartProtection = 90;
+      FanTrailingTimer = 90;
+    }
   }
 
-}
+  if (FAN_REQUS != FAN_ON)
+  {
+    // FAN ON to OFF only when  COMP is off AND trailing timer 0
+    if (FAN_REQUS == false && COMP_ON == false && FanTrailingTimer == 0)
+    {
+      FAN_ON = false;
+    }
+    else
+    {
+      FAN_ON = true;
+    }
+  }
 
+  if (FAN_ON)
+  {
+    digitalWrite(DeHum.pin_fanh, HIGH);
+    delay(5);
+    digitalWrite(DeHum.pin_fanl, LOW);
+  }
+  else
+  {
+    digitalWrite(DeHum.pin_fanh, LOW);
+    delay(5);
+    digitalWrite(DeHum.pin_fanl, LOW);
+  }
+      
+  if (COMP_ON)
+  {
+      // Set Compressor to ON
+      digitalWrite(DeHum.pin_comp, HIGH);
+  }
+  else
+  {
+          // Set Compressor to OFF
+      digitalWrite(DeHum.pin_comp, LOW);
+  }
+
+
+}
 
 void DeHumShow(void)
 {
   ResponseAppend_P(PSTR("{\"FAN_SPEED\": %i}"), FAN_SPEED);
-  ResponseAppend_P(PSTR("{\"COMPRESSOR\": %i"), COMP_ON);
+  ResponseAppend_P(PSTR("{\"COMPRESSOR\": %i}"), COMP_ON);
   ResponseAppend_P(PSTR("{\"WATER\": %i"), water_full);
   ResponseJsonEnd();
 }
@@ -251,17 +292,24 @@ bool Xdrv102(uint32_t function)
       // Select suitable interval for polling your function
     case FUNC_EVERY_SECOND:
         if (FanTrailingTimer > 0)
-          FanTrailingTimer--;
+        {
+          FanTrailingTimer --;
+        }
+        else
+        {
+          FanTrailingTimer = 0;
+        }
+          
 
-        // FAN timer reached 1, so disable fan and set 0
-        if (FanTrailingTimer == 1)
-          {
-            digitalWrite(DeHum.pin_fanh, LOW);
-            digitalWrite(DeHum.pin_fanl, LOW);
-            FanTrailingTimer = 0;
-            FAN_ON = false;
-            FAN_SPEED = 0;
-          }
+        if (restartProtection > 0)
+        {
+          restartProtection --;
+        }
+        else
+        {
+          restartProtection = 0;
+        }
+
     break;
 //      case FUNC_EVERY_250_MSECOND:
 //    case FUNC_EVERY_200_MSECOND:
